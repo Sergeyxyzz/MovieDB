a1299affa3ddf18a26ffa587c9ee502aaaedd24d
Sergey <sergeyxyzz@yandex.ru>
2021-08-15 13:00:39 +0300

    sort and change array films

--- js/script.js
+++ js/script.js
@@ -24,13 +24,39 @@ const movieDB = {
     ]
 };
 
+// 1
 const deleteAdd = document.querySelectorAll('.delete')
 deleteAdd.forEach(it => {
     it.remove()
 })
 
+// 2
 const promo__genre = document.querySelector('.promo__genre')
 promo__genre.textContent = 'ДРАМА'
 
+// 3
 const promo__bg = document.querySelector('.promo__bg')
 promo__bg.style.cssText = `background: url('img/bg.jpg'); background-size: cover`
+
+// 4 & 5
+const list = document.querySelector('.promo__interactive-list')
+const films = document.querySelectorAll('.promo__interactive-item')
+
+films.forEach(it => {
+    it.remove()
+})
+
+movieDB.movies.sort()
+
+movieDB.movies.forEach((it,i) => {
+    const div = document.createElement('div')
+    div.innerHTML = `${++i} ${it}`
+    list.append(div)
+    div.classList.add('promo__interactive-item')
+})
+
+
+
+
+
+
